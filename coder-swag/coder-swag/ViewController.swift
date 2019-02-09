b5fc6141eb865ce1b1c798d9d9e1c2454b586d1f
Finn Kiesinger <fkiesinger@me.com>
2019-02-09 17:09:22 +0100

    empty project

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Finn Kiesinger on 09.02.19.
+//  Copyright © 2019 Finn Kiesinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
